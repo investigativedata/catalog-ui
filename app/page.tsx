10bb288c6c1e0e32f60f58d771ada602fd217b55
kjacks <kjack227@gmail.com>
2024-04-11 15:54:02 +0200

    Temporarily set home page to fetch ctalogs locally

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
 import Page from "~/components/Page";
 import CatalogScreen from "~/screens/CatalogScreen";
 import { CATALOG_URI } from "~/settings";
-import { getCatalog } from "@investigativedata/ftmq";
+// import { getCatalog } from "@investigativedata/ftmq";
+
+import catalog from '../public/investigraph.eu.json'
 
 const breadcrumbs = [
   {
@@ -10,7 +12,7 @@ const breadcrumbs = [
 ];
 
 export default async function CatalogPage() {
-  const catalog = await getCatalog(CATALOG_URI);
+  // const catalog = await getCatalog(CATALOG_URI);
   return (
     <Page crumbs={breadcrumbs}>
       <CatalogScreen catalog={catalog} />
