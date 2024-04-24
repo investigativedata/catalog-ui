ffdfe11ccd48e6304a481c0c5799874249ba4b09
kjacks <kjack227@gmail.com>
2024-04-24 16:59:02 +0200

    Implemented basic query functionality

--- components/Catalog.tsx
+++ components/Catalog.tsx
@@ -1,12 +1,12 @@
-import type { ICatalog } from "@investigativedata/ftmq";
+import type { ICatalog, IDataset } from "@investigativedata/ftmq";
 import Grid from "@mui/joy/Grid";
 
 import CatalogItem from "./CatalogItem";
 
-export default function Catalog({ catalog }: { catalog: ICatalog }) {
+export default function Catalog({ datasets }: { datasets: IDataset[] }) {
   return (
    <Grid container spacing={2} sx={{ flexGrow: 1 }}>
-     {catalog.datasets?.map((d) => (
+     {datasets?.map((d) => (
        <Grid key={d.name} xs={12} md={6}>
         <CatalogItem key={d.name} dataset={d} />
        </Grid>
--- screens/CatalogScreen.tsx
+++ screens/CatalogScreen.tsx
@@ -1,27 +1,49 @@
 "use client";
 
-import type { ICatalog } from "@investigativedata/ftmq";
-import List from "@mui/joy/List";
-import ListItem from "@mui/joy/ListItem";
+import { useEffect, useState } from "react";
+import type { ICatalog, IDataset } from "@investigativedata/ftmq";
 import Stack from "@mui/joy/Stack";
 import Grid from '@mui/joy/Grid';
 import Typography from "@mui/joy/Typography";
-import Link from "next/link";
+import Fuse from "fuse.js";
 
 import Catalog from "~/components/Catalog";
 import Search from "~/components/Search";
 import FilterGroup from "~/components/FilterGroup";
 
+const initializeSearchIndex = (items: IDataset[]) => {
+  return new Fuse(items, {
+    keys: ["title", "summary"],
+  });
+}
+
 export default function CatalogScreen({ catalog }: { catalog: ICatalog }) {
+  const searchIndex = initializeSearchIndex(catalog?.datasets)
+  const [searchValue, setSearchValue] = useState('');
+  const [filteredItems, setFilteredItems] = useState([])
+  
+  useEffect(() => {
+    console.log('changed', searchValue)
+
+    if (searchValue.length === 0) {
+      setFilteredItems(catalog.datasets)
+    } else {
+      const results = searchIndex.search(searchValue)
+        .map((result) => result.item);
+
+      setFilteredItems(results)
+    }
+  }, [searchValue]);
+
   return (
     <Stack>
       <Grid container spacing={2} sx={{ flexGrow: 1 }}>
         <Grid xs={3}>
-          <Search />
+          <Search value={searchValue} setValue={setSearchValue} />
           <FilterGroup />
         </Grid>
         <Grid xs={6}>
-          <Catalog catalog={catalog} />
+          <Catalog datasets={filteredItems} />
         </Grid>
         <Grid xs={3}>
           <Typography level="h3">Welcome to the Investigraph Data Catalog.</Typography>
