592ea2316b8994cc6297a8845581d2771b245d6b
kjacks <kjack227@gmail.com>
2024-04-24 16:59:13 +0200

    Added Search input component

--- components/Search.tsx
+++ components/Search.tsx
@@ -1,5 +1,19 @@
-export type TSearch = { };
+import Input from '@mui/joy/Input';
 
-export default function Search({  }: {  }) {
-  return null
+export type TSearch = {
+  value: string,
+  setValue: (value: string) => void
+};
+
+export default function Search({ value, setValue }): TSearch {
+  return (
+    <Input
+      color="neutral"
+      variant="plain"
+      size="sm"
+      placeholder="Search"
+      value={value}
+      onChange={(event) => setValue(event.target.value)}
+    />
+  )
 }
