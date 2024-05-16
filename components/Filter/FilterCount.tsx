ce1cd922f23504bf402c32d493280a8dc2f42a6b
kjacks <kjack227@gmail.com>
2024-05-16 12:32:23 +0200

    Add new filter icon for desktop

--- components/Filter/FilterCount.tsx
+++ components/Filter/FilterCount.tsx
@@ -1,14 +1,15 @@
 import Chip from "@mui/joy/Chip";
 import ChipDelete from '@mui/joy/ChipDelete';
-import Typography from "@mui/joy/Typography";
+import IconButton from "@mui/joy/IconButton";
 
 export type TFilterCount = {
   value: number,
   verbose?: boolean,
+  withIcon?: boolean,
   onClear: () => void
 };
 
-export default function FilterCount({ onClear, value, verbose }: TFilterCount) {
+export default function FilterCount({ onClear, value, verbose, withIcon }: TFilterCount) {
   if (value === 0) {
     return null;
   }
@@ -30,10 +31,30 @@ export default function FilterCount({ onClear, value, verbose }: TFilterCount) {
         height: "2rem",
         backgroundColor: theme.vars.palette.common.black,
         color: theme.vars.palette.common.white,
-        padding: "2px 8px"
+        padding: "2px 8px",
       })}
+      slotProps={{
+        label: {
+          sx:{
+            display: "flex",
+            alignItems: "center",
+            gap: "3px"
+          }
+        }
+      }}
     >
       <span>{value}</span>
+      {withIcon && (
+        <IconButton sx={{ display: { xs: "none", sm: "inline-flex" }, minWidth: "0", padding: "0" }} disabled>
+          <img
+            src="/static/icons/filter_white.svg"
+            style={{ 
+              width: "1rem",
+              height: "1rem"
+            }}
+          />
+        </IconButton>
+      )}
       {verbose && <span> selected</span>}
     </Chip>
   )
