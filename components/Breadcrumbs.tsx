8fc67c792d4cc3194980e6612e42de2336a04825
kjacks <kjack227@gmail.com>
2024-05-22 13:00:58 +0200

    Breadcrumbs styling

--- components/Breadcrumbs.tsx
+++ components/Breadcrumbs.tsx
@@ -7,14 +7,21 @@ export type TBreadrumb = { label: string | React.ReactNode; url?: string };
 export default function Breadcrumbs({ crumbs }: { crumbs: TBreadrumb[] }) {
   if (crumbs.length == 0) return null;
   return (
-    <MuiBreadcrumbs sx={{ padding: 0, fontSize: "sm" }}>
+    <MuiBreadcrumbs 
+      sx={{ padding: 0 }}
+      slotProps={{ 
+        li: { 
+          sx: theme => ({ color: theme.palette.common.black, fontSize: "sm" })
+        }
+      }}
+    >
       {crumbs.map(({ label, url }, ix) =>
         url ? (
           <Link key={ix} href={url} >
             {label}
           </Link>
         ) : (
-          <Typography sx={{ fontSize: "inherit" }} key={ix}>
+          <Typography key={ix}>
             {label}
           </Typography>
         )
